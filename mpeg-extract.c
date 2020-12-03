ca65073256d4f7a006aee1d5d25378e225806e2f
jky <joko.yuliantoro@gmail.com>
2020-12-03 16:45:45 +0400

    Add HTTP header removal.

--- mpeg-extract.c
+++ mpeg-extract.c
@@ -15,10 +15,13 @@ int main(int argc, char **argv) {
   char *fno = NULL;
   struct pcap_pkthdr hdr;
   int i;
+  int j;
   struct in_addr ip4a;
   char *ip4src;
+  char *ip4src_in;
   pcap_t *pf;
   const u_char *p;
+  const u_char *p_payload;
 
   FILE *fp;
   int iphdrlen;
@@ -28,13 +31,16 @@ int main(int argc, char **argv) {
   int payloadlen;
   int tcphdrlen;
 
-  while ((c = getopt(argc, argv, "f:o:")) != -1) {
+  while ((c = getopt(argc, argv, "f:o:s:")) != -1) {
     switch (c) {
       case 'f':
         fni = optarg;
         break;
       case 'o':
         fno = optarg;
+        break;
+      case 's':
+        ip4src_in = optarg;
         break;
       default:
         fprintf(stderr, "Err: Unknown option: -%c\n", optopt);
@@ -52,8 +58,14 @@ int main(int argc, char **argv) {
     return 0;
   }
 
+  if (ip4src_in == NULL) {
+    fprintf(stderr, "Err: Need IPv4 source address identifier using -s.\n");
+    return 0;
+  }
+
   printf("input filename: %s\n", fni);
   printf("output filename: %s\n", fno);
+  printf("ipv4 source: %s\n", ip4src_in);
 
   pf = pcap_open_offline(fni, errbuf);
 
@@ -74,14 +86,30 @@ int main(int argc, char **argv) {
 
     ip4a.s_addr = p[0x1a] + (p[0x1b]<<8) + (p[0x1c]<<16) + (p[0x1d]<<24);
     ip4src = inet_ntoa(ip4a);
-    if (i>4 && !strcmp(ip4src, "185.132.179.104")) {
+    if (i>4 && !strcmp(ip4src, ip4src_in)) {
       iphdrlen = (p[0x0e] & 0x0f) << 2;
       iplen = (p[0x10]<<8) + p[0x11];
       tcphdrlen = (p[0x2e] & 0xf0) >> 2;
       payloadlen = iplen - iphdrlen - tcphdrlen;
+      p_payload = p + l2hdrlen + iphdrlen + tcphdrlen;
+
+      if (p_payload[0]=='H' && p_payload[1]=='T' && p_payload[2]=='T' && p_payload[3]=='P') {
+        printf("%i: found http header, checking the real payload..\n", i);
+        const u_char *p_real_payload = p_payload;
+        j = 0;
+        while (j < (payloadlen-4)) {
+          p_real_payload++;
+          if (p_real_payload[0]==0x0d && p_real_payload[1]==0x0a && p_real_payload[2]==0x0d && p_real_payload[3]==0x0a) {
+            p_payload = p_real_payload + 4;
+            payloadlen -= j;
+            j = payloadlen;
+          }
+          j++;
+        }
+      }
 
       if (iplen <= MAX_PAYLOAD_LEN) {
-        memcpy(payload, p + l2hdrlen + iphdrlen + tcphdrlen, payloadlen);
+        memcpy(payload, p_payload, payloadlen);
         payload[payloadlen+1] = 0;
         fwrite(payload, payloadlen, 1, fp);
       } else {
